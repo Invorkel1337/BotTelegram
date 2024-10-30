150589ae53871978e40c7d4019efec8508386a53
Invorkel1337 <semen03112001@list.ru>
2024-10-31 02:07:33 +0800

    d

---
+++ src/main/java/com/ToDoBot/TodoBot.java
@@ -0,0 +1,11 @@
+package com.ToDoBot;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TodoBot {
+
+}
