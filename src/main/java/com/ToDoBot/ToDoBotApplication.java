2488dd5c89394498aeebf893bd7743010da8fbc8
Invorkel1337 <semen03112001@list.ru>
2024-11-02 00:56:46 +0800

    Чото пишет уже

---
+++ src/main/java/com/ToDoBot/ToDoBotApplication.java
@@ -0,0 +1,10 @@
+package com.ToDoBot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ToDoBotApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(ToDoBotApplication.class, args);
+        }
+}
+
--- src/main/java/com/ToDoBot/TodoBot.java
+++ src/main/java/com/ToDoBot/TodoBot.java
@@ -6,6 +6,40 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class TodoBot {
-int piska = 21;
+@Component
+public class TodoBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "TodoListHelperBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7813592997:AAFkxkZSJxXWp9HTJZB28k4gigsQu79iXjw";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            if (messageText.startsWith("/start")) {
+                sendMessage(chatId, "Добро пожаловать в To-Do List Bot!");
+            }
+        }
+    }
+
+    private void sendMessage(long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
