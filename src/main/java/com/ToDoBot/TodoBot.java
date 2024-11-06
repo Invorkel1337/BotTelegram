a2e5ec48dd36450b9f21b1a0a476544e9a84611b
Invorkel1337 <semen03112001@list.ru>
2024-11-07 01:43:00 +0800

    /edittask

--- src/main/java/com/ToDoBot/TodoBot.java
+++ src/main/java/com/ToDoBot/TodoBot.java
@@ -14,7 +14,7 @@ public class TodoBot extends TelegramLongPollingBot {
     @Autowired
     TaskService taskService;
 
-    //Автоматом сделалось без него не работает
+    //Автоматом сделалось без него не работает1
     @Override
     public String getBotUsername() {
         return "TodoListHelperBot";
